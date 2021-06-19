ba7599f5861049a430a4a91fad3b773b9adf2008
faizanpro77 <faizyshaikh204@gmail.com>
2021-06-19 23:14:08 +0530

    MODIFIED: add functionality to calculate aggregate total for multiple rides

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class InvoiceServiceTest {
     @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+    public void givenDistanceAndTime_shouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
@@ -14,11 +14,20 @@ public class InvoiceServiceTest {
     }
 
     @Test
-    public void givenDistanceOrTime_ShouldReturnMinFare() {
+    public void givenDistanceOrTime_shouldReturnMinFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_shouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0,5),
+                        new Ride(0.1, 1)};
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30, fare, 0.0);
     }
 }
